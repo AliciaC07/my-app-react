1882359330f72bbd5157c3bca19d48a3be380a53
Alicia <aliciacruzj0307@outlook.com>
2023-11-17 18:10:32 -0400

    Testing react.

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/RH-book1.jpg';
+import img2 from './images/second_book.jpg';
+import img3 from './images/3_book.jpg';
+const firstBook = {
+  title: "Reminders of Him: A Novel",
+  author: "Colleen Hoover",
+  img: img1,
+  id: 1,
+};
+const secondBook = {
+  title: "The Second Life of Mirielle West",
+  author: "Amanda Skenandore",
+  img: img2,
+  id: 2,
+};
+
+const therdBook = {
+  title: "The Family Across the Street",
+  author: "Nicole Trope",
+  img: img3,
+  id: 3,
+};
+
+const booksList = [firstBook, secondBook, therdBook];
+export default booksList;
